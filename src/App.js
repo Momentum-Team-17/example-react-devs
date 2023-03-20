5277454d3fdf39ffe5a1ee1134777402404bcd6c
Amy Gori <amydgori@gmail.com>
2023-03-20 11:52:36 -0400

    Add AJAX request for dev repo data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import 'bulma/bulma.sass'
 import './index.scss'
 import './App.css'
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 function App({ devs, greeting }) {
   return (
     <div className="container">
@@ -10,8 +10,8 @@ function App({ devs, greeting }) {
         <h1 className="is-size-1">React Devs for Hire</h1>
       </header>
       <div className="block">
-        {devs.map((dev) => {
-          return <Developer dev={dev} />
+        {devs.map((dev, index) => {
+          return <Developer dev={dev} key={index} />
         })}
       </div>
     </div>
@@ -20,7 +20,17 @@ function App({ devs, greeting }) {
 
 function Developer({ dev }) {
   const [expanded, setExpanded] = useState(false)
+  const [repos, setRepos] = useState(null)
 
+  // useEffect is called after each render
+  useEffect(() => {
+      console.log("useEffect runs")
+      const URL=`https://api.github.com/users/${dev.githubName}/repos`
+    // we're using JS Promises
+    axios.get(URL).then((response) => setRepos(response.data) )
+  }, [])
+
+  console.log("Developer Component Renders")
   return (
     <div className="card m-4">
       <div className="card-content">
@@ -33,7 +43,16 @@ function Developer({ dev }) {
             Show {expanded ? 'less' : 'more'}
           </button>
         </div>
-        {expanded && <p className="content m-3">{dev.expertise}</p>}
+        {expanded && (
+          <>
+            <p className="content m-3">{dev.expertise}</p>
+            <div className="repo-list">
+              <ul>
+                {repos && repos.map(repo => <li>{repo.name}</li>) }
+              </ul>
+            </div>
+          </>
+          )}
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 const devs = [
-  { name: 'Gerardo', expertise: 'JS, photography' },
-  { name: 'Emmaline', expertise: 'JS, sewing' },
-  { name: 'Chris', expertise: 'dropping things, JS' },
-  { name: 'Michael', expertise: 'music, JS' },
+  { name: 'Gerardo', expertise: 'JS, photography', githubName: "GerardoRoman" },
+  { name: 'Emmaline', expertise: 'JS, sewing', githubName: "esaintb" },
+  { name: 'Chris', expertise: 'dropping things, JS', githubName: "chriscors" },
+  { name: 'Michael', expertise: 'music, JS', githubName: "MichaelGreason" },
 ]
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
